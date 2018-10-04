2158b62fde1f8f4458fba9c85a8c4c5f9a9505c7
INTENS\miodrag.vukicevic <miodrag.vukicevic@intens.rs>
2018-10-04 15:16:09 +0200

    removed css

--- src/app/prva.component.ts
+++ src/app/prva.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prva',
-  templateUrl: './prva.component.html',
-  styleUrls: ['./prva.component.css']
+  templateUrl: './prva.component.html'
 })
 export class PrvaComponent implements OnInit {
 
